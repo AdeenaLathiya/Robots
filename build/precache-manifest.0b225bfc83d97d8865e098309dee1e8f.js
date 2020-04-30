80e3a0bab5bb77472b47ea4ec873403a52a1b14e
root <root@linux.www.mtlinkwifi.com>
2020-05-01 01:23:46 +0500

    First Commit

---
+++ build/precache-manifest.0b225bfc83d97d8865e098309dee1e8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af97da7f88fe930b44dd0d3a14999fc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "766b7e5122fbe4b76510",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "fba08c874431db9a3f68",
+    "url": "/static/css/main.56576c76.chunk.css"
+  },
+  {
+    "revision": "766b7e5122fbe4b76510",
+    "url": "/static/js/2.4e0c0955.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4e0c0955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fba08c874431db9a3f68",
+    "url": "/static/js/main.db70740f.chunk.js"
+  },
+  {
+    "revision": "36b4f3e2aba21b9ce3f9",
+    "url": "/static/js/runtime-main.79f88b1e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
