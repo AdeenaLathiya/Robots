b9b944078d7f0363009f2d96786130e68164062c
root <root@linux.www.mtlinkwifi.com>
2020-05-01 15:32:45 +0500

    Updates

---
+++ precache-manifest.8ddbe4af77b616f2338251eb4cc8edcd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c123dcb513ce975002c1a2d1bdccad62",
+    "url": "/Robots/index.html"
+  },
+  {
+    "revision": "766b7e5122fbe4b76510",
+    "url": "/Robots/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "7be2a360c30068368aef",
+    "url": "/Robots/static/css/main.f409e8c4.chunk.css"
+  },
+  {
+    "revision": "766b7e5122fbe4b76510",
+    "url": "/Robots/static/js/2.4e0c0955.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robots/static/js/2.4e0c0955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7be2a360c30068368aef",
+    "url": "/Robots/static/js/main.78da607e.chunk.js"
+  },
+  {
+    "revision": "4b948e8c7600170aecd6",
+    "url": "/Robots/static/js/runtime-main.c069163a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
